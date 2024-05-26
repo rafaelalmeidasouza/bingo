4f9dd0d49b7e18728de0349cbba6c703f0e98d1e
Rafael Souza <41439851+rafaelalmeidasouza@users.noreply.github.com>
2024-05-26 14:26:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Array para armazenar os números já sorteados
+let numerosSorteados = [];
+
+// Função para sortear um número
+function sortearNumero() {
+    let numeroSorteado;
+
+    // Verificar se todos os números foram sorteados
+    if (numerosSorteados.length < 75) {
+        // Sortear um número aleatório que ainda não foi sorteado
+        do {
+            numeroSorteado = Math.floor(Math.random() * 75) + 1;
+        } while (numerosSorteados.includes(numeroSorteado));
+
+        // Adicionar o número sorteado ao array de números sorteados
+        numerosSorteados.push(numeroSorteado);
+
+        // Atualizar o número sorteado na página
+        document.getElementById('numero-sorteado').innerText = 'Número Sorteado: ' + numeroSorteado;
+    } else {
+        alert('Todos os números já foram sorteados!');
+    }
+}
+
+// Adicionar um evento de clique ao botão de sorteio
+document.getElementById('sortear-btn').addEventListener('click', sortearNumero);
